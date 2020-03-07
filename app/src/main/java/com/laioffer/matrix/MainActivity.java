c6d09326a8e760d38f7f9ac87c84e46b2f4cd2d3
Menglin Yu <yml.100205@gmail.com>
2020-03-06 23:17:04 -0500

    initial commit

---
+++ app/src/main/java/com/laioffer/matrix/MainActivity.java
@@ -0,0 +1,58 @@
+package com.laioffer.matrix;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements ListFragment.OnItemSelectListener, GridFragment.OnItemSelectListener {
+    private ListFragment listFragment;
+    private GridFragment gridFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add list view
+        if (getSupportFragmentManager().findFragmentById(R.id.list_container) == null) {
+            listFragment = new ListFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.list_container, listFragment).commit();
+        }
+
+        //add Gridview
+        if (getSupportFragmentManager().findFragmentById(R.id.grid_container) == null && isTablet()) {
+            gridFragment = new GridFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.grid_container, gridFragment).commit();
+        }
+
+    }
+    //监听list fragment里面的变化，应该给与什么反应
+    //当list fragment被点击，grid fragment应该选择对应的item
+    // Add this method to main activity
+    @Override
+    public void onItemSelected(int position) {
+        if (!isTablet()) {
+            Fragment fragment = GridFragment.newInstance(position);
+            getSupportFragmentManager().beginTransaction().replace(R.id.list_container, fragment).addToBackStack(null).commit();
+        } else {
+            gridFragment.onItemSelected(position);
+        }
+    }
+
+    @Override
+    public void onCommentSelected(int position) {
+        listFragment.onItemSelected(position);
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+}
---
+++ lib/src/main/java/com/laioffer/lib/MyClass.java
@@ -0,0 +1,4 @@
+package com.laioffer.lib;
+
+public class MyClass {
+}
